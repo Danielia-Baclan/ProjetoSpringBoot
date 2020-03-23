d452c9327c8f29de2bef7e7b661b80e0fe63739e
Danielia-Baclan <jeniffer_baclan@hotmail.com>
2020-03-23 17:13:36 -0300

    Inicio

---
+++ src/test/java/br/edu/up/casa/CasaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.up.casa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
